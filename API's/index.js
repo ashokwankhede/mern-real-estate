b5f1b2e2180330dcc08369aad0531876eab9373e
Ashok Wankhede <wankhedeashok9822@gmail.com>
2024-02-21 00:23:06 +0530

    created database and connected to the express js

--- API's/index.js
+++ API's/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.configDotenv();
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("Connected to the MongoDB!")
+})
+.catch((error)=>{
+    console.log("Please check the password string!")
+    console.log(error);
+});
 
 const app = express();
 const port = 3000;
